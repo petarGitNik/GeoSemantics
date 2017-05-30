a93a0d5bf56b3a306fead96ccabe1b1ed3c261ec
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-05-30 13:16:30 +0200

    Remove colored output

---
+++ geosemantics.py
@@ -0,0 +1,114 @@
+from SPARQLWrapper import SPARQLWrapper
+from SPARQLWrapper import JSON
+
+from sys import argv
+from sys import exit
+
+"""
+geosemantics latitude longitude tolerance [language]
+"""
+
+# If there are too many arguments - exit
+if len(argv) > 6:
+    print "Too many arguments."
+    exit(0)
+
+# If there are not enough arguments - exit
+if len(argv) < 4:
+    print "Not enough arguments."
+    exit(0)
+
+def is_it_a_number(candidate):
+    try:
+        float(candidate)
+    except ValueError:
+        print "Input values must be numbers!"
+        return False
+    return True
+
+# If any of the arguments is not a number - exit
+for value in [argv[1], argv[2], argv[3]]:
+    if not is_it_a_number(value):
+        exit(0)
+
+latitude = float(argv[1])
+longitude = float(argv[2])
+tolerance = float(argv[3])
+
+# If there is no input value for language, default to whatever dbpedia returns
+language = 'default'
+if len(argv) == 5:
+    language = argv[4]
+
+# If default value for language is used, then do not use language filters
+def set_language_filters(language):
+    if language == 'default':
+        return ['', '']
+    filter_lang_label = "filter(lang(?label) = \'" + language + "\')\n"
+    filter_lang_comment = "filter(lang(?comment) = \'" + language + "\')\n"
+    return [filter_lang_label, filter_lang_comment]
+
+# Relevant code starts here
+sparql = SPARQLWrapper('http://dbpedia.org/sparql')
+
+# Prepare boundaries
+top = latitude + tolerance
+bottom = latitude - tolerance
+left = longitude - tolerance
+right = longitude + tolerance
+
+# Prepare filters
+filter_latitude = "?lat > " + str(bottom) + " && ?lat < " + str(top)
+filter_longitude = "?long > " + str(left) + " && ?long < " + str(right)
+
+filter_lang_label, filter_lang_comment = set_language_filters(language)
+
+sparql.setQuery("""
+    PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX yago: <http://dbpedia.org/class/yago/>
+
+    SELECT DISTINCT
+    ?name ?lat ?long (MIN(?label) AS ?label) (MIN(?comment) AS ?comment)
+    WHERE {
+        ?name rdf:type yago:Company108058098.
+        ?name geo:lat ?lat.
+        ?name geo:long ?long.
+
+        filter(""" + filter_latitude + """ && """ + filter_longitude + """)
+        OPTIONAL {
+        ?name rdfs:label ?label.
+        ?name rdfs:comment ?comment.
+
+        """ + filter_lang_label + filter_lang_comment + """
+        }
+    }
+    GROUP BY ?name ?lat ?long
+    LIMIT 20
+""")
+
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+
+def get_value_of_key(dictionary, key, value):
+    if dictionary.get(key, False):
+        return unicode(dictionary[key][value])
+    else:
+        return 'No value found in dbpedia'
+
+for result in results['results']['bindings']:
+    name = unicode(result['name']['value'])
+
+    label = get_value_of_key(result, 'label', 'value')
+    comment = get_value_of_key(result, 'comment', 'value')
+
+    latitude = str(result['lat']['value'])
+    longitude = str(result['long']['value'])
+
+    print '\n'
+    print 'Name:', name
+    print 'Label:', label
+    print 'Comment:', comment
+    print 'Latitude:', latitude
+    print 'longitude:', longitude + '\n'
